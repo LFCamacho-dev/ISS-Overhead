71eadfa7d4e8e32c66293ce6cf66ae8a6da807b3
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-28 15:08:34 -0600

    sends email every 60 secs when the ISS is in range, and it's dark outside

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import requests
 from datetime import datetime, timezone
+import time
+import smtplib
+
+# region MAIL AUTH
+email = "*********@gmail.com"
+password = "***************"
+SMTP_SERVER = "smtp.gmail.com"
+PORT = 587
+SSL_PORT = 465
+send_to = "**********@hotmail.com"
+# endregion MAIL AUTH
 
 MY_LAT = 32.820194
 MY_LONG = -96.784688
+start_time = time.time()
 
 parameters = {"lat": MY_LAT, "lng": MY_LONG, "formatted": 0}
 
@@ -14,10 +26,11 @@ def check_proximity():
 
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
-    iss_latitude = 29.795
-    iss_longitude = -96.78
-    print(f"ISS LAT: {iss_latitude}")
-    print(f"ISS LON: {iss_longitude}")
+    # iss_latitude = 29.795  # for testing
+    # iss_longitude = -96.78  # for testing
+
+    print(f"ISS Latitude: {iss_latitude}, My Latitude: {MY_LAT}")
+    print(f"ISS Longitude: {iss_longitude}, My Longitude: {MY_LONG}")
 
     # Your position is within +5 or -5 degrees of the ISS position.
     lat_check = -5 <= (iss_latitude - MY_LAT) <= 5
@@ -25,14 +38,12 @@ def check_proximity():
 
     return lat_check and lon_check
 
-def check_if_night():
 
-    print(MY_LAT)
-    print(MY_LONG)
+def check_if_night():
     response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
     response.raise_for_status()
     data = response.json()
-    print(data)
+    # print(data)
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
@@ -40,13 +51,23 @@ def check_if_night():
     return (time_now.hour <= sunrise) or (time_now.hour >= sunset)
 
 
+while True:
+    if check_proximity():
+        print("The ISS is in range!")
+        if not check_if_night(): print("...but it's too bright out there to see it :(")
+        elif check_if_night():
+            print("Look up! you should be able to see it, sending email...")
 
-if check_proximity():
-    print("both values are true")
-    if check_if_night():
-        print("it's dark outside")
-    else: print("too bright to see the ISS")
-# If the ISS is close to my current position
-# ,and it is currently dark
-# Then email me to tell me to look up.
-# BONUS: run the code every 60 seconds.
+            with smtplib.SMTP_SSL(SMTP_SERVER, SSL_PORT) as connection:
+                connection.login(user=email, password=password)
+                connection.sendmail(
+                    from_addr=email,
+                    to_addrs=send_to,
+                    msg=f"Subject: LOOK UP!\n\n "
+                        f"The ISS is right above you, look up!")
+
+            print("Email sent :D")
+
+    else: print("The ISS is too far to see...")
+
+    time.sleep(60.0 - ((time.time() - start_time) % 60.0))
