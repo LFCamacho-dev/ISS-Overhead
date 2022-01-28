49b425d65066199ed31aaecb42514d9081b66791
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-28 13:52:05 -0600

    checking if it's dark enough to see the ISS in the sky

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
 
-MY_LAT = 32.7942144
-MY_LONG = -96.7835648
+MY_LAT = 32.820194
+MY_LONG = -96.784688
+
+parameters = {"lat": MY_LAT, "lng": MY_LONG, "formatted": 0}
 
 
 def check_proximity():
@@ -10,40 +12,41 @@ def check_proximity():
     response.raise_for_status()
     data = response.json()
 
-    # iss_latitude = float(data["iss_position"]["latitude"])
-    # iss_longitude = float(data["iss_position"]["longitude"])
-    iss_latitude = 27.795
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = 29.795
     iss_longitude = -96.78
     print(f"ISS LAT: {iss_latitude}")
     print(f"ISS LON: {iss_longitude}")
 
-    #Your position is within +5 or -5 degrees of the ISS position.
+    # Your position is within +5 or -5 degrees of the ISS position.
     lat_check = -5 <= (iss_latitude - MY_LAT) <= 5
-    # print(f"{lat_check}: {iss_latitude - MY_LAT}")
     lon_check = -5 <= (iss_longitude - MY_LONG) <= 5
 
     return lat_check and lon_check
 
+def check_if_night():
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+    print(MY_LAT)
+    print(MY_LONG)
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    print(data)
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now(timezone.utc)
+    return (time_now.hour <= sunrise) or (time_now.hour >= sunset)
 
-time_now = datetime.now()
 
-print(check_proximity())
+
+if check_proximity():
+    print("both values are true")
+    if check_if_night():
+        print("it's dark outside")
+    else: print("too bright to see the ISS")
 # If the ISS is close to my current position
-# and it is currently dark
+# ,and it is currently dark
 # Then email me to tell me to look up.
 # BONUS: run the code every 60 seconds.
-
-
-
